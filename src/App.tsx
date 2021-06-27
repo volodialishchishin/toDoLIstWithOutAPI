4cae20c096a7cf390c94f22962a2df3287addc2b
volodya <lishchishin.volodya@gmail.com>
2021-06-27 14:01:38 +0300

    Fix menu

--- src/App.tsx
+++ src/App.tsx
@@ -81,12 +81,10 @@ function App() {
             <AppBar position="static">
                 <Toolbar>
                     <IconButton edge="start" color="inherit" aria-label="menu">
-                        <Menu/>
                     </IconButton>
                     <Typography variant="h6">
-                        News
                     </Typography>
-                    <Button color="inherit">Login</Button>
+                    <Button color="inherit"></Button>
                 </Toolbar>
             </AppBar>
             <Container fixed>
